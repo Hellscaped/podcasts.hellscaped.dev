0903188afdf44ac28987f39fa9595994ff389a30
Hellscaped <tappsyo@gmail.com>
2024-04-04 11:43:21 -0500

    real inital commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# podcasts.hellscaped.dev
-occasionally I have podcasts I'm forced to do at school so why not throw them on a public site
+# school podcasts
+
+trash podcasts for school
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [coins, setCoins] = useState(0)
+
+  return (
+    <>
+      <video id="thefireislooping" controls autoPlay loop="true">
+        <source src="/the_fire_is_looping_V1.mp4" type="video/mp4" />
+      </video>
+      <div className="podcast vcr">
+        <h1>GREED PODCAST</h1>
+        <h2>made for my ELA class</h2>
+        <div className="card">
+          <h2>4-1: SLAVES TO POWER</h2>
+          <h3>GREED /// FIRST</h3>
+          <audio controls>
+            <source src="/greed_first.m4a" type="audio/mp4" />
+          </audio>
+          <button id='coin' onClick={() => setCoins(coins + 1)}>
+            {coins}
+          </button>
+          <p>
+
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
